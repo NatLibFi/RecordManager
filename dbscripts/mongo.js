0a3f3ed1f5545360a3db9c4744ca033e129e040b
Ere Maijala <ere.maijala@helsinki.fi>
2012-02-17 14:53:59 +0200

    Added source_id index.

--- dbscripts/mongo.js
+++ dbscripts/mongo.js
@@ -3,3 +3,4 @@ db.record.ensureIndex({title_keys: 1});
 db.record.ensureIndex({isbn_keys: 1});
 db.record.ensureIndex({oai_id: 1});
 db.record.ensureIndex({host_record_id: 1});
+db.record.ensureIndex({source_id: 1});
