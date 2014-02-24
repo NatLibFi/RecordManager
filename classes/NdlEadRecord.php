c367003285d24627a01be5f728eba1c3888e29f3
Ere Maijala <ere.maijala@helsinki.fi>
2014-02-24 11:38:40 +0200

    [ALLI-2473] NdlEadRecord: improved date range parsing with support for dd.mm.yyyy format.

--- classes/NdlEadRecord.php
+++ classes/NdlEadRecord.php
@@ -101,10 +101,7 @@ class NdlEadRecord extends EadRecord
             return null;
         }
 
-        if (preg_match('/(\d\d\d\d) ?- ?(\d\d\d\d)/', $input, $matches) > 0) {
-            $startDate = $matches[1] . '-01-01T00:00:00Z';
-            $endDate = $matches[2] . '-12-31T00:00:00Z';
-        } elseif (preg_match('/(\d\d?).(\d\d?).(\d\d\d\d) ?- ?(\d\d?).(\d\d?).(\d\d\d\d)/', $input, $matches) > 0) {
+        if (preg_match('/(\d\d?).(\d\d?).(\d\d\d\d) ?- ?(\d\d?).(\d\d?).(\d\d\d\d)/', $input, $matches) > 0) {
             $startYear = $matches[3];
             $startMonth = sprintf('%02d', $matches[2]);
             $startDay = sprintf('%02d', $matches[1]);
@@ -129,10 +126,19 @@ class NdlEadRecord extends EadRecord
                 return null;
             }
             $endDate = $d->format('Y-m-t') . 'T23:59:59Z';
+        } elseif (preg_match('/(\d\d\d\d) ?- ?(\d\d\d\d)/', $input, $matches) > 0) {
+            $startDate = $matches[1] . '-01-01T00:00:00Z';
+            $endDate = $matches[2] . '-12-31T00:00:00Z';
         } elseif (preg_match('/(\d\d\d\d)-(\d\d?)-(\d\d?)/', $input, $matches) > 0) {
             $year = $matches[1];
             $month =  sprintf('%02d', $matches[2]);
             $day = sprintf('%02d', $matches[3]);
+            $startDate = $year . '-' . $month . '-' .  $day . 'T00:00:00Z';
+            $endDate = $year . '-' . $month . '-' .  $day . 'T23:59:59Z';
+        } elseif (preg_match('/(\d\d?).(\d\d?).(\d\d\d\d)/', $input, $matches) > 0) {
+            $year = $matches[3];
+            $month =  sprintf('%02d', $matches[2]);
+            $day = sprintf('%02d', $matches[1]);
             $startDate = $year . '-' . $month . '-' .  $day . 'T00:00:00Z';
             $endDate = $year . '-' . $month . '-' .  $day . 'T23:59:59Z';
         } elseif (preg_match('/(\d\d?)\.(\d\d\d\d)/', $input, $matches) > 0) {
