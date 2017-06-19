0bbf37a7a6e97b7b7444f9d8d1c40e52054c19f3
konstara <konsta.raunio@helsinki.fi>
2017-06-19 15:29:23 +0300

    [ALLI-2985] Added check for un-labeled displayObjects to show in summaryas well as those with label 'aihe'.

--- classes/NdlLidoRecord.php
+++ classes/NdlLidoRecord.php
@@ -480,10 +480,19 @@ class NdlLidoRecord extends LidoRecord
         $subjectDescriptions = [];
         foreach ($this->getSubjectSetNodes() as $set) {
             if (mb_strtolower($set->displaySubject['label'], 'UTF-8') == 'aihe') {
-                $subjectDescriptions[] = (string) $set->displaySubject;
+                $subjectDescriptions[] = (string)$set->displaySubject;
+            } elseif (empty($set->displaySubject['label'])
+                && !empty($set->displaySubject)
+            ) {
+                $subjectDescriptions[] = (string)$set->displaySubject;
             }
         }
-
+        if ($subjectDescriptions
+            && $this->getTitle() == implode('; ', $subjectDescriptions)
+        ) {
+            // We have the description already in the title, don't repeat
+            $subjectDescriptions = [];
+        }
         return trim(
             implode(
                 ' ', array_merge($descriptionWrapDescriptions, $subjectDescriptions)
