525ab92aaca7cd9291b03ef28b2c05359494ffb3
Ere Maijala <ere.maijala@helsinki.fi>
2017-06-27 09:18:30 +0300

    Separate export to its own class.

--- export.php
+++ export.php
@@ -4,7 +4,7 @@
  *
  * PHP version 5
  *
- * Copyright (C) The National Library of Finland 2011-2013.
+ * Copyright (C) The National Library of Finland 2011-2017.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -67,12 +67,11 @@ EOT;
         exit(1);
     }
 
-    $manager = new \RecordManager\Base\Controller\RecordManager(
+    $export = new \RecordManager\Base\Controller\Export(
         true, isset($params['verbose']) ? $params['verbose'] : false
     );
-    $manager->quiet = isset($params['quiet']) ? $params['quiet'] : false;
 
-    $manager->exportRecords(
+    $export->launch(
         $params['file'],
         isset($params['deleted']) ? $params['deleted'] : '',
         isset($params['from']) ? $params['from'] : '',
