a7bc421d8d88adb6ed488e3f4e100ca3bf608aa9
jlehmus <jukka.lehmus@helsinki.fi>
2017-07-25 18:37:22 +0300

    more

--- src/RecordManager/Finna/Record/Ead3.php
+++ src/RecordManager/Finna/Record/Ead3.php
@@ -4,6 +4,18 @@ namespace RecordManager\Finna\Record;
 use RecordManager\Base\Utils\Logger;
 use RecordManager\Base\Utils\MetadataUtils;
 
+/**
+ * Ead3 record class
+ *
+ * This is a class for processing EAD records.
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @author   Jukka Lehmus
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/jlehmus/RecordManager
+ */
 class Ead3 extends \RecordManager\Base\Record\Base
 {
     protected $doc = null;
@@ -99,6 +111,9 @@ class Ead3 extends \RecordManager\Base\Record\Base
                 }
             }
         }
+
+        $genre = $doc->xpath('controlaccess/genreform/part');
+        $data['format'] = (string) ($genre ? $genre[0] : $doc->attributes()->level);
 
         switch ($data['format']) {
         case 'fonds':
@@ -230,9 +245,6 @@ class Ead3 extends \RecordManager\Base\Record\Base
             }
             $data['topic'] = $data['topic_facet'] = $topics;
         }
-
-        $genre = $doc->xpath('controlaccess/genreform/part');
-        $data['format'] = (string) ($genre ? $genre[0] : $doc->attributes()->level);
 
         // Single-valued sequence for sorting
         if (isset($data['hierarchy_sequence'])) {
