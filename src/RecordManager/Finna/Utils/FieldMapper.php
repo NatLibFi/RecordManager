e719cc9684b522dcb8ffbf63ff50805fb84ba6d5
Ere Maijala <ere.maijala@helsinki.fi>
2018-09-05 14:57:06 +0300

    [ALLI-5589] Finna: Add Field Mapper.

---
+++ src/RecordManager/Finna/Utils/FieldMapper.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * Field value mapper
+ *
+ * PHP version 5
+ *
+ * Copyright (C) The National Library of Finland 2018.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/KDK-Alli/RecordManager
+ */
+namespace RecordManager\Finna\Utils;
+
+/**
+ * Field value mapper
+ *
+ * @category DataManagement
+ * @package  RecordManager
+ * @author   Ere Maijala <ere.maijala@helsinki.fi>
+ * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
+ * @link     https://github.com/KDK-Alli/RecordManager
+ */
+class FieldMapper extends \RecordManager\Base\Utils\FieldMapper
+{
+    /**
+     * Mapping file cache
+     *
+     * @var array
+     */
+    protected static $mapCache = [];
+
+    /**
+     * Settings for all data sources
+     *
+     * @var array
+     */
+    protected $settings = [];
+
+    /**
+     * Constructor
+     *
+     * @param string $basePath           Base path for configuration files
+     * @param array  $defaultMappings    Default mappings for all data sources
+     * @param array  $dataSourceSettings Data source settings
+     */
+    public function __construct($basePath, $defaultMappings, $dataSourceSettings)
+    {
+        parent::__construct($basePath, $defaultMappings, $dataSourceSettings);
+
+        foreach ($this->settings as $source => &$settings) {
+            if (empty($settings['mappingFiles']['format_ext_str_mv'])
+                && !empty($settings['mappingFiles']['format'])
+            ) {
+                $settings['mappingFiles']['format_ext_str_mv']
+                    = $settings['mappingFiles']['format'];
+            }
+        }
+    }
+
+    /**
+     * Map source format to Solr format
+     *
+     * @param string $source Source ID
+     * @param string $format Format
+     *
+     * @return string Mapped format string
+     */
+    public function mapFormat($source, $format)
+    {
+        $settings = $this->settings[$source];
+
+        if (isset($settings['mappingFiles']['format'])) {
+            $mappingFile = $settings['mappingFiles']['format'];
+            $map = $mappingFile[0]['map'];
+            if (!empty($format)) {
+                $format = $this->mapValue($format, $mappingFile);
+                return is_array($format) ? $format[0] : $format;
+            } elseif (isset($map['##empty'])) {
+                return $map['##empty'];
+            } elseif (isset($map['##emptyarray'])) {
+                return $map['##emptyarray'];
+            }
+        }
+        return $format;
+    }
+
+    /**
+     * Map all fields in an array
+     *
+     * @param string $source Source ID
+     * @param array  $data   Fields to process
+     *
+     * @return array
+     */
+    public function mapValues($source, $data)
+    {
+        $settings = $this->settings[$source];
+        foreach ($settings['mappingFiles'] as $field => $mappingFile) {
+            if (isset($data[$field]) && !empty($data[$field])) {
+                if (is_array($data[$field])) {
+                    $newValues = [];
+                    foreach ($data[$field] as $value) {
+                        $replacement = $this->mapValue($value, $mappingFile);
+                        if (is_array($replacement)) {
+                            $newValues = array_merge($newValues, $replacement);
+                        } else {
+                            $newValues[] = $replacement;
+                        }
+                    }
+                    if (null !== $newValues) {
+                        $data[$field] = array_values(array_unique($newValues));
+                    }
+                } else {
+                    $data[$field] = $this->mapValue($data[$field], $mappingFile);
+                }
+            } elseif (isset($mappingFile[0]['map']['##empty'])) {
+                $data[$field] = $mappingFile[0]['map']['##empty'];
+            } elseif (isset($mappingFile[0]['map']['##emptyarray'])) {
+                $data[$field] = [$mappingFile[0]['map']['##emptyarray']];
+            }
+        }
+        return $data;
+    }
+
+    /**
+     * Map a value using a mapping file
+     *
+     * @param mixed $value       Value to map
+     * @param array $mappingFile Mapping file
+     * @param int   $index       Mapping index for sub-entry mappings
+     *
+     * @return mixed
+     */
+    protected function mapValue($value, $mappingFile, $index = 0)
+    {
+        if (is_array($value)) {
+            // Map array parts (predefined hierarchy) separately
+            $newValue = [];
+            foreach ($value as $i => $v) {
+                $v = $this->mapValue($v, $mappingFile, $i);
+                if ('' === $v) {
+                    // If we get an empty string from any level, stop here
+                    break;
+                }
+                $newValue[] = $v;
+            }
+            return implode('/', $newValue);
+        }
+        $map = isset($mappingFile[$index]['map']) ? $mappingFile[$index]['map']
+            : $mappingFile[0]['map'];
+        $type = isset($mappingFile[$index]['type'])
+            ? $mappingFile[$index]['type'] : $mappingFile[0]['type'];
+        if ('regexp' == $type) {
+            foreach ($map as $pattern => $replacement) {
+                $pattern = addcslashes($pattern, '/');
+                $newValue = preg_replace(
+                    "/$pattern/u", $replacement, $value, -1, $count
+                );
+                if ($count > 0) {
+                    return $newValue;
+                }
+            }
+            return $value;
+        }
+        $replacement = $value;
+        if (isset($map[$value])) {
+            $replacement = $map[$value];
+        } elseif (isset($map['##default'])) {
+            $replacement = $map['##default'];
+        }
+        return $replacement;
+    }
+
+    /**
+     * Read a mapping file (two strings separated by ' = ' per line)
+     *
+     * @param string $filename Mapping file name
+     *
+     * @throws Exception
+     * @return array Mappings
+     */
+    protected function readMappingFile($filename)
+    {
+        $mappings = [];
+        $handle = fopen($filename, 'r');
+        if (!$handle) {
+            throw new \Exception("Could not open mapping file '$filename'");
+        }
+        $lineno = 0;
+        while (($line = fgets($handle))) {
+            ++$lineno;
+            $line = rtrim($line);
+            if (!$line || $line[0] == ';') {
+                continue;
+            }
+            $values = explode(' = ', $line, 2);
+            if (!isset($values[1])) {
+                if (strstr($line, ' =') === false) {
+                    fclose($handle);
+                    throw new \Exception(
+                        "Unable to parse mapping file '$filename' line "
+                        . "(no ' = ' found): ($lineno) $line"
+                    );
+                }
+                $values = explode(' =', $line, 2);
+                $mappings[$values[0]] = '';
+            } else {
+                $key = trim($values[0]);
+                if (substr($key, -2) == '[]') {
+                    $mappings[substr($key, 0, -2)][] = $values[1];
+                } else {
+                    $mappings[$key] = $values[1];
+                }
+            }
+        }
+        fclose($handle);
+        return $mappings;
+    }
+}
