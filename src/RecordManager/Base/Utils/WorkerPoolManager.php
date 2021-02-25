f2532ade7e4712fc8c2e58be4692abe5b6cd3e2c
Ere Maijala <ere.maijala@helsinki.fi>
2021-02-25 11:24:17 +0200

    WPM: Limit number of retries when socket call gets interrupted.

--- src/RecordManager/Base/Utils/WorkerPoolManager.php
+++ src/RecordManager/Base/Utils/WorkerPoolManager.php
@@ -405,17 +405,18 @@ class WorkerPoolManager
     {
         $msgLen = '';
         $received = 0;
+        $interrupted = 0;
         do {
             if ($checkParent) {
                 $this->checkParentIsAlive();
             }
             $read = [$socket];
-            $write = null;
-            $except = null;
+            $write = [];
+            $except = [];
             $res = socket_select($read, $write, $except, $block ? 1000 : 0);
             if (false === $res) {
                 $error = socket_last_error();
-                if (SOCKET_EINTR === $error) {
+                if (SOCKET_EINTR === $error || (++$interrupted < 10)) {
                     // Retry after an interrupted system call
                     continue;
                 }
@@ -430,6 +431,7 @@ class WorkerPoolManager
                 usleep(10);
                 continue;
             }
+            $interrupted = 0;
 
             $buffer = '';
             $result = socket_recv(
@@ -498,9 +500,9 @@ class WorkerPoolManager
             if ($checkParent) {
                 $this->checkParentIsAlive();
             }
-            $read = null;
+            $read = [];
             $write = [$socket];
-            $except = null;
+            $except = [];
             $res = socket_select($read, $write, $except, null);
             if (false === $res) {
                 throw new \Exception(
