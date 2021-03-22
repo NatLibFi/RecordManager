a5bae5c2268726bd6d9b37c9a94b857da815ad1c
Ere Maijala <ere.maijala@helsinki.fi>
2021-03-22 09:40:01 +0200

    Mark records deleted 1000 at a time.

--- src/RecordManager/Base/Controller/MarkDeleted.php
+++ src/RecordManager/Base/Controller/MarkDeleted.php
@@ -75,7 +75,7 @@ class MarkDeleted extends AbstractBase
         do {
             // Fetch a set of records at a time since the remaining set will be
             // changing during this process.
-            $records = $this->db->findRecords($params);
+            $records = $this->db->findRecords($params, ['limit' => 1000]);
             $more = false;
             foreach ($records as $record) {
                 $more = true;
