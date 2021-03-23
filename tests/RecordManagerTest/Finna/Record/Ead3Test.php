41415a722847abdccb9764a26af35b8fccc5fbc3
Samuli Sillanpää <samuli.sillanpaa@helsinki.fi>
2021-03-23 13:42:20 +0200

    Fix EAD3 test namespace.

--- tests/RecordManagerTest/Finna/Record/Ead3Test.php
+++ tests/RecordManagerTest/Finna/Record/Ead3Test.php
@@ -25,7 +25,7 @@
  * @license  http://opensource.org/licenses/gpl-2.0.php GNU General Public License
  * @link     https://github.com/NatLibFi/RecordManager
  */
-namespace RecordManagerTest\Base\Record;
+namespace RecordManagerTest\Finna\Record;
 
 use RecordManager\Finna\Record\Ead3;
 
