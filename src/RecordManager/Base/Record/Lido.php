f8ef6bb51f40f516725256f7809819f7f4385530
LuomaJuha <juhaluoma5@gmail.com>
2021-11-25 12:35:14 +0200

    Adjust comments

--- src/RecordManager/Base/Record/Lido.php
+++ src/RecordManager/Base/Record/Lido.php
@@ -220,11 +220,11 @@ class Lido extends AbstractRecord
     /**
      * Return record title
      *
-     * @param bool     $forFiling            Whether the title is to be used in
+     * @param bool   $forFiling            Whether the title is to be used in
      *                                       filing (e.g. sorting, non-filing
      *                                       characters should be removed)
-     * @param string   $lang                 Language
-     * @param array    $excludedDescriptions Description types to exclude
+     * @param string $lang                 Language
+     * @param array  $excludedDescriptions Description types to exclude
      *
      * @return string
      */
