6941043bdd4655f7cb8715150b9758ac73d08ee1
mshroom <32199029+mshroom@users.noreply.github.com>
2022-05-17 13:23:35 +0300

    [ALLI-6423] EAD: Take language from langcode attribute if available (#107)

--- src/RecordManager/Base/Record/Ead.php
+++ src/RecordManager/Base/Record/Ead.php
@@ -234,9 +234,12 @@ class Ead extends AbstractRecord
             'UTF-8'
         );
 
-        if ($languages = $doc->did->xpath('langmaterial/language')) {
-            $data['language'] = $this->metadataUtils
-                ->normalizeLanguageStrings($languages);
+        foreach ($doc->did->langmaterial ?? [] as $langmaterial) {
+            foreach ($langmaterial->language ?? [] as $lang) {
+                $l = $lang->attributes()->langcode ?? $lang;
+                $data['language'][] = $this->metadataUtils
+                    ->normalizeLanguageStrings($l);
+            }
         }
 
         if ($extents = $doc->did->xpath('physdesc/extent')) {
