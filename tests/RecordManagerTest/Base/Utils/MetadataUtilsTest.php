f4008f3520deb2db6e60d24b40c2fa79f4e9d6e4
Ere Maijala <ere.maijala@helsinki.fi>
2022-11-15 14:09:55 +0200

    Round coordinate precision in tests to avoid issues with higher precision.

--- tests/RecordManagerTest/Base/Utils/MetadataUtilsTest.php
+++ tests/RecordManagerTest/Base/Utils/MetadataUtilsTest.php
@@ -183,10 +183,13 @@ class MetadataUtilsTest extends \PHPUnit\Framework\TestCase
             'E0793235.575' => 79.54321527777778,
         ];
 
+        // Test by rounding to lowest precision for PHP 8 compatibility
+        // (see https://github.com/php/php-src/blob/PHP-8.0/UPGRADING#L584#L587):
+        $precision = min(ini_get('precision'), ini_get('serialize_precision')) ?: 15;
         foreach ($values as $from => $to) {
             $this->assertEquals(
-                $to,
-                $this->metadataUtils->coordinateToDecimal($from),
+                is_string($to) ? $to : round($to, $precision),
+                round($this->metadataUtils->coordinateToDecimal($from), $precision),
                 $from
             );
         }
