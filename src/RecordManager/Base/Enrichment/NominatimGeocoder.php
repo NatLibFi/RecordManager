96ee2b1244675c509eac9e2601d722b3ae7e84e6
Ere Maijala <ere.maijala@helsinki.fi>
2023-03-07 13:20:21 +0200

    Nominatim: handle missing WKT and other fields properly.

--- src/RecordManager/Base/Enrichment/NominatimGeocoder.php
+++ src/RecordManager/Base/Enrichment/NominatimGeocoder.php
@@ -4,7 +4,7 @@
  *
  * PHP version 7
  *
- * Copyright (C) The National Library of Finland 2013-2022.
+ * Copyright (C) The National Library of Finland 2013-2023.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
@@ -406,9 +406,9 @@ class NominatimGeocoder extends AbstractEnrichment
                 continue;
             }
             $items[] = [
-                'wkt' => $place['geotext'],
-                'lat' => $place['lat'],
-                'lon' => $place['lon'],
+                'wkt' => $place['geotext'] ?? '',
+                'lat' => $place['lat'] ?? '',
+                'lon' => $place['lon'] ?? '',
                 'importance' => $importance
             ];
         }
