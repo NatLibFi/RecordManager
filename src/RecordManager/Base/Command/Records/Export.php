388f3f2996ada79cd6b7fbcfc6c460e7d04ef496
Ere Maijala <ere.maijala@helsinki.fi>
2023-10-05 15:58:01 +0300

    Add support for injecting record id's to exported records.

--- src/RecordManager/Base/Command/Records/Export.php
+++ src/RecordManager/Base/Command/Records/Export.php
@@ -164,6 +164,16 @@ class Export extends AbstractBase
                 "Whether to include dedup id's in exported records. Supported"
                 . ' values: deduped = if duplicates exist, always = always. '
                 . " Default is to not include the dedup id's."
+            )->addOption(
+                'inject-id',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Inject record ID without source prefix to the given XML field'
+            )->addOption(
+                'inject-id-prefixed',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Inject record ID with source prefix to the given XML field'
             );
     }
 
@@ -190,6 +200,8 @@ class Export extends AbstractBase
         $sortDedup = $input->getOption('sort-dedup');
         $addDedupId = $input->getOption('dedup-id');
         $this->batchSize = $input->getOption('batch-size') ?: 0;
+        $injectId = $input->getOption('inject-id');
+        $injectIdPrefixed = $input->getOption('inject-id-prefixed');
 
         $returnCode = Command::SUCCESS;
 
@@ -293,7 +305,10 @@ class Export extends AbstractBase
                     $skipRecords,
                     $xpath,
                     $deletedFile,
-                    $addDedupId
+                    $addDedupId,
+                    $injectId,
+                    $injectIdPrefixed,
+                    $sourceId
                 ) {
                     $metadataRecord = $this->createRecord(
                         $record['format'],
@@ -301,22 +316,50 @@ class Export extends AbstractBase
                         $record['oai_id'],
                         $record['source_id']
                     );
-                    if ($xpath) {
-                        $xml = $metadataRecord->toXML();
+                    if (!$record['deleted']) {
+                        if ($addDedupId == 'always') {
+                            $metadataRecord->addDedupKeyToMetadata(
+                                $record['dedup_id']
+                                ?? $record['_id']
+                            );
+                        } elseif ($addDedupId == 'deduped') {
+                            $metadataRecord->addDedupKeyToMetadata(
+                                $record['dedup_id']
+                                ?? ''
+                            );
+                        }
+                    }
+                    $xml = $metadataRecord->toXML();
+                    if ($xpath || (($injectId || $injectIdPrefixed) && !$record['deleted'])) {
                         $dom = $this->metadataUtils->loadXML($xml);
                         if (!$dom) {
                             throw new \Exception(
-                                "Failed to parse record '${$record['_id']}'"
+                                "Failed to parse record '{$record['_id']}'"
                             );
                         }
-                        $xpathResult = $dom->xpath($xpath);
-                        if ($xpathResult === false) {
-                            throw new \Exception(
-                                "Failed to evaluate XPath expression '$xpath'"
-                            );
+                        if ($xpath) {
+                            $xpathResult = $dom->xpath($xpath);
+                            if ($xpathResult === false) {
+                                throw new \Exception(
+                                    "Failed to evaluate XPath expression '$xpath'"
+                                );
+                            }
+                            if (!$xpathResult) {
+                                return true;
+                            }
                         }
-                        if (!$xpathResult) {
-                            return true;
+                        if (!$record['deleted']) {
+                            if ($injectId) {
+                                $dom->addChild($injectId, htmlspecialchars($metadataRecord->getID(), ENT_NOQUOTES));
+                                $xml = $dom->saveXML();
+                            }
+                            if ($injectIdPrefixed) {
+                                $dom->addChild(
+                                    $injectIdPrefixed,
+                                    htmlspecialchars($sourceId . '.' . $metadataRecord->getID(), ENT_NOQUOTES)
+                                );
+                                $xml = $dom->saveXML();
+                            }
                         }
                     }
                     ++$count;
@@ -347,7 +390,6 @@ class Export extends AbstractBase
                                 ?? ''
                             );
                         }
-                        $xml = $metadataRecord->toXML();
                         $xml = preg_replace('/^<\?xml.*?\?>[\n\r]*/', '', $xml);
                         $this->writeRecord($xml);
                     }
