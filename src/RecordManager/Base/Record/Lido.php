1c06201aed5e998a585b781cfb60de6f7d2ed444
Minna Rönkä <32199029+mshroom@users.noreply.github.com>
2026-04-24 10:27:20 +0300

    Update src/RecordManager/Base/Record/Lido.php
    Co-authored-by: Ere Maijala <ere.maijala@helsinki.fi>

--- src/RecordManager/Base/Record/Lido.php
+++ src/RecordManager/Base/Record/Lido.php
@@ -1559,6 +1559,7 @@ class Lido extends AbstractRecord
      */
     protected function addHierarchyTitles(array &$data): void
     {
+        // Note: title_in_hierarchy is only needed if it differs from title.
         if ($this->getDriverParam('addIdToHierarchyTitle', true)) {
             $data['title_in_hierarchy'] = trim($this->getIdentifier() . ' ' . $data['title']);
         }
